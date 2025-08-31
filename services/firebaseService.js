8655f1ce905c24e5efe84a7a5e8d57ace3975cd3
Devollox <122895078+Devollox@users.noreply.github.com>
2025-08-31 14:00:17 +0300

    Update firebaseService.js

--- services/firebaseService.js
+++ services/firebaseService.js
@@ -4,7 +4,7 @@ import { get, getDatabase, ref, runTransaction } from 'firebase/database'
 const app = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  databaseURL: 'https://devollodb-default-rtdb.europe-west1.firebasedatabase.app',
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -31,3 +31,4 @@ export const incrementVisitorCount = async slug => {
     return current + 1
   })
 }
+
